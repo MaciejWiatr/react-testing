ada0969f920d4e91744e30bceed7bc99acb03faa
Dettlaff <maciekwiatr17@gmail.com>
2020-07-02 16:13:37 +0200

    Initial components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./components/header";
+import "./app.scss";
 
 function App() {
-    return <div className="App">test</div>;
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
